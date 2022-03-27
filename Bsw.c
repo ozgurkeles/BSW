f9d8417359ce295b86b23edc321eebb9c7c87f14
Özgür Keleş <ozgur.keles@polirons.com>
2022-03-27 03:54:42 +0300

    Add BSW

---
+++ Bsw.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "Bsw.h"
+
+void Bsw_Start(void)
+{
+    printf("BSW start\n");
+}
---
+++ Bsw.h
@@ -0,0 +1,14 @@
+#ifndef BSW_H
+#define BSW_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void Bsw_Start(void);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif /* __cplusplus */
+
+#endif // BSW_H
